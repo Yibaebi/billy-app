994b609e3aaa9da6471c80924db36faf37bae7f7
yibaebi <elliot.yibaebi@yahoo.com>
2025-07-13 21:05:13 +0100

    setup react-query and update expo packages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,12 +2,15 @@ import '@/global.css';
 import 'react-native-reanimated';
 
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 
 import FONTS from '@/constants/Fonts';
 import { useColorScheme } from '@/hooks/useColorScheme';
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -17,13 +20,15 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack initialRouteName="dashboard">
-        <Stack.Screen name="dashboard" options={{ headerShown: false }} />
-        <Stack.Screen name="onboarding" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+      <QueryClientProvider client={queryClient}>
+        <Stack initialRouteName="dashboard">
+          <Stack.Screen name="dashboard" options={{ headerShown: false }} />
+          <Stack.Screen name="onboarding" options={{ headerShown: false }} />
+          <Stack.Screen name="+not-found" />
+        </Stack>
 
-      <StatusBar animated />
+        <StatusBar animated />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
